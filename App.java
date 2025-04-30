9df4abc16506128348e6c3e72042de27b2178392
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-04-30 20:00:36 +0100

    Create the boilerplate for the app launcher

---
+++ App.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public void start(Stage stage) {
+        VBox root = new VBox(100);
+        Scene scene = new Scene(root, 800, 650);
+
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
