1d1836303c16da29cd97ecf91c8c90e86ad98cec
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-03 12:26:34 +0100

    Created a new file for storing the main page, and seperated the logic of the page so that it is not too large and confusing

--- App.java
+++ App.java
@@ -13,50 +13,23 @@ import javafx.scene.text.Font;
 public class App extends Application {
 
     public void start(Stage stage) {
-                VBox root = new VBox(100);
+        VBox root = new VBox(100);
         Scene scene = new Scene(root, 800, 650);
-        HBox topBox = new HBox(50);
-        ScrollPane listPane = new ScrollPane();
-        VBox listBox = new VBox(50);
-        HBox labelWrapper = new HBox(20);
+          HBox labelWrapper = new HBox(20);
         Label label = new Label("Notes app!");
         label.setFont(new Font("Ariel", 20));
         labelWrapper.getChildren().add(label);
         labelWrapper.setAlignment(Pos.CENTER);
         root.getChildren().add(labelWrapper);
         
+        VBox main = Base.top();
 
-        TextField text = new TextField("");
-        Button addButton = new Button("Add note to list");
-        addButton.setOnAction(e -> {
-            String userText = text.getCharacters().toString();
-            if (!userText.isEmpty()) {
-                HBox innerBox = new HBox(10);
-                Label innerText = new Label(userText);
-                Button deleteButton = new Button("X");
-                deleteButton.setOnAction(er -> {
-                    listBox.getChildren().remove(innerBox);
-                });
-                innerBox.getChildren().addAll(innerText, deleteButton);
-                text.clear();
-
-                listBox.getChildren().addFirst(innerBox);
-                listPane.layout();
-                listPane.setVvalue(0);
-            }
-        });
-
-        topBox.setAlignment(Pos.CENTER);
-
-        topBox.getChildren().addAll(text, addButton);
-
-        listPane.setContent(listBox);
-
-        root.getChildren().addAll(topBox, listPane);
+        root.getChildren().addAll(main);
 
         stage.setScene(scene);
 
-        stage.show();    }
+        stage.show(); 
+    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ Base.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.text.Font;
+
+public class Base{
+  public static VBox top()
+  {
+
+          VBox root = new VBox(100);
+        HBox topBox = new HBox(50);
+        ScrollPane listPane = new ScrollPane();
+        VBox listBox = new VBox(50);
+        
+
+        TextField text = new TextField("");
+        Button addButton = new Button("Add note to list");
+        addButton.setOnAction(e -> {
+            String userText = text.getCharacters().toString();
+            if (!userText.isEmpty()) {
+                HBox innerBox = new HBox(10);
+                Label innerText = new Label(userText);
+                Button deleteButton = new Button("X");
+                deleteButton.setOnAction(er -> {
+                    listBox.getChildren().remove(innerBox);
+                });
+                innerBox.getChildren().addAll(innerText, deleteButton);
+                text.clear();
+
+                listBox.getChildren().addFirst(innerBox);
+                listPane.layout();
+                listPane.setVvalue(0);
+            }
+        });
+
+        topBox.setAlignment(Pos.CENTER);
+
+        topBox.getChildren().addAll(text, addButton);
+
+        listPane.setContent(listBox);
+
+        root.getChildren().addAll(topBox, listPane);
+
+        return root;
+  }
+}
