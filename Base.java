d71626ae17fbcf3642e5702e234e801f25c78253
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-05 10:01:06 +0100

    Making the styling blue

--- Base.java
+++ Base.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -19,6 +20,7 @@ public class Base {
 
     public static VBox top() {
         VBox root = new VBox(20);
+        root.setStyle("-fx-background-color: #e6f7ff;");
 
         HBox topBox = new HBox(10);
         topBox.setAlignment(Pos.CENTER);
@@ -50,6 +52,7 @@ public class Base {
         Label titleLabel = new Label("Default Title");
         titleLabel.setFont(new Font(20));
         titleLabel.setAlignment(Pos.CENTER);
+        titleLabel.setTextFill(Color.DARKBLUE);
         VBox.setVgrow(titleLabel, Priority.ALWAYS);
 
         VBox titleContainer = new VBox();
@@ -73,6 +76,7 @@ public class Base {
             });
             titleBox.getChildren().addAll(titleField, setTitleButton);
             Scene titleScene = new Scene(titleBox, 300, 150);
+            titleScene.getRoot().setStyle("-fx-background-color: #e6f7ff;");
             titleStage.setScene(titleScene);
             titleStage.show();
         });
